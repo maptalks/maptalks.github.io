6e8a298a2628adf58d63f4f78d609b9079b1959e
fuzhenn <fuzhen@maptalks.org>
2017-05-26 18:25:13 +0800

    updates

--- src/script/main.js
+++ src/script/main.js
@@ -1,3 +1,49 @@
+var map;
+var bearing = -14.2;
 $(function() {
-    console.log("main entry");
-})
+  var features = topojson.feature(world, world.objects.land);
+  var polygons = features.features.map(function (f) {
+    var geo = maptalks.GeoJSON.toGeometry(f);
+    geo.setId(null);
+    geo.config('antiMeridian', 'split');
+    geo.setSymbol({
+      lineColor : 'rgba(255, 255, 255, 1)',
+      lineWidth : 1
+    });
+    return geo;
+  });
+  map = new maptalks.Map('map', {
+    center:  [-2.7903012612708142, 60.976349249268345],
+    zoom: 3,
+    pitch: 60,
+    bearing: bearing,
+    draggable: false,
+    scrollWheelZoom: false,
+    geometryEvents:false,
+    hitDetect:false,
+    zoomable: false,
+    layers: [
+      new maptalks.VectorLayer('v', polygons, { opacity : 0.3 }),
+      new maptalks.VectorLayer('m', [
+          new maptalks.MultiPoint([[-0.12755, 51.507222], [37.6178, 55.7517], [28.9744, 41.0128], /*[12.5, 41.9], [7.4458, 46.95]*//*, [116.391212, 39.893409]*/])
+        ])
+    ]
+  });
+
+  rotate();
+});
+
+var rotation = 8;
+var duration = 4000;
+var rotationPerFrame = 8 / (duration / 16);
+var current = 0;
+var step = 0;
+var d = 1;
+function rotate() {	
+	if (Math.abs(step * rotationPerFrame) >= rotation) {		
+		d *= -1;
+	}
+	step += d;
+	map.setBearing(bearing + step * rotationPerFrame);
+	requestAnimationFrame(rotate);
+}
